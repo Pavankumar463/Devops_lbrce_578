27511b0ccfa3401604dfb37d5be68570f7b9ea7b
PAVAN KUMAR REDDY <PAVAN KUMAR REDDY@LAPTOP-CJKLPFBO>
2024-11-21 17:28:23 +0530

    Commit

---
+++ src/project_pavan_bogasamudram/file1.java
@@ -0,0 +1,12 @@
+package project_pavan_bogasamudram;
+import java.util.*;
+public class file1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str1=sc.next();
+		System.out.println(str1);
+
+	}
+
+}
