55c62cd4401eabda7e71a6c84ee6f563faec794a
PAVAN KUMAR REDDY <PAVAN KUMAR REDDY@LAPTOP-CJKLPFBO>
2024-11-22 10:59:49 +0530

    Commit

---
+++ src/project_pavan_bogasamudram/Fisrtprogram.java
@@ -0,0 +1,12 @@
+package project_pavan_bogasamudram;
+
+import java.util.*;
+public class Fisrtprogram {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		System.out.println(s);
+	}
+
+}
