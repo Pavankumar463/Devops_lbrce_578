44f7bd67d976fec3b6e91ff7d5969da893b18992
PAVAN KUMAR REDDY <PAVAN KUMAR REDDY@LAPTOP-CJKLPFBO>
2024-11-22 11:28:14 +0530

    Commit

--- src/project_pavan_bogasamudram/file2.java
+++ src/project_pavan_bogasamudram/file2.java
@@ -1,10 +1,57 @@
 package project_pavan_bogasamudram;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class file2 {
+    public static void main(String[] args) {
+        // Stores two numbers
+        double num1, num2;
 
-	public static void main(String[] args) {
-		System.out.println("Hello woprld");
+        // Read input from a file (you can replace "input.txt" with the actual file path)
+        try {
+            File file = new File("input.txt");
+            Scanner sc = new Scanner(file);
 
-	}
+            System.out.println("Reading input from file:");
 
+            // Take the inputs
+            num1 = sc.nextDouble();
+            num2 = sc.nextDouble();
+
+            // Read the operator
+            char op = sc.next().charAt(0);
+            double o = 0;
+
+            // Perform calculation based on operator
+            switch (op) {
+                case '+':
+                    o = num1 + num2;
+                    break;
+                case '-':
+                    o = num1 - num2;
+                    break;
+                case '*':
+                    o = num1 * num2;
+                    break;
+                case '/':
+                    o = num1 / num2;
+                    break;
+                default:
+                    System.out.println("You entered an invalid operator.");
+                    return;
+            }
+
+            // Print the result
+            System.out.println("The final result:");
+            System.out.println(num1 + " " + op + " " + num2 + " = " + o);
+
+            // Close the file scanner
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
