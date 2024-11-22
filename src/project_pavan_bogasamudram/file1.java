b8cc9f5b52f99305d743c2ba83b4eaba407d2039
PAVAN KUMAR REDDY <PAVAN KUMAR REDDY@LAPTOP-CJKLPFBO>
2024-11-22 12:08:03 +0530

    Commmit

--- src/project_pavan_bogasamudram/file1.java
+++ src/project_pavan_bogasamudram/file1.java
@@ -1,12 +1,43 @@
 package project_pavan_bogasamudram;
-import java.util.*;
+import java.util.Scanner;
+
 public class file1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		String str1=sc.next();
-		System.out.println(str1);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
 
-	}
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
 
+        System.out.print("Enter the operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+    }
 }
